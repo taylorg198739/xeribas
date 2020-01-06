97cad2bb30015ac156a2b9a0ba05d413e40699e5
taylor <taylorg198739@gmail.com>
2020-01-06 13:55:33 -0500

    Initial commit

---
+++ js/test.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+var  mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
